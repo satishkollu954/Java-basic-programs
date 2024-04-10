20a661089e32c3a0d7b6d2a85cf272691e7653ef
satishkollu954 <satishkollu954@gmail.com>
2024-04-10 23:11:44 +0530

    First commit

---
+++ src/main/java/org/example/EligibleForVote.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class EligibleForVote {
+    public static void main(String[] args) {
+        int n=17;
+        if(n>=18)
+        {
+            System.out.println("eligible for vote");
+        }
+        else
+        {
+            System.out.println("not eligible for vote");
+        }
+    }
+}
