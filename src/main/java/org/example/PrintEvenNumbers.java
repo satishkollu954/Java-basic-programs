fa5fcd1650432006124745e9f6f399240d9486f9
satishkollu954 <satishkollu954@gmail.com>
2024-04-10 23:22:25 +0530

    Second commit

---
+++ src/main/java/org/example/PrintEvenNumbers.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class PrintEvenNumbers {
+    public static void main(String[] args) {
+    for(int i=0;i<=10;i++)
+    {
+        if(i%2==0)
+        {
+            System.out.println(i);
+        }
+    }
+    }
+}
