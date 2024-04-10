13f96157b7babb019ff035165e57caf7fcfdd065
satishkollu954 <satishkollu954@gmail.com>
2024-04-10 23:14:56 +0530

    Second commit

---
+++ src/main/java/org/example/EvenOrOdd.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int n=8;
+        if(n%2==0)
+        {
+            System.out.println("even number");
+        }
+        else
+        {
+            System.out.println("odd number");
+        }
+    }
+}
